5da801de1abf002f506910f8f30c039ea8dc64af
Fedyboq <166071546+Fedyboq@users.noreply.github.com>
2025-05-11 10:55:47 -0500

    Update ingesta.py

--- ingesta.py
+++ ingesta.py
@@ -1,9 +1,11 @@
 import boto3
 
-ficheroUpload = "data.csv"
-nombreBucket = "gcr-output-01"
+ficheroUpload = "usuarios.csv"
+ficheroUpload2 = "direccion.csv"
+nombreBucket = "prdocutos-cloud-pp"
 
 s3 = boto3.client('s3')
-response = s3.upload_file(ficheroUpload, nombreBucket, ficheroUpload)
+response = s3.upload_file(ficheroUpload, nombreBucket, "usuarios/usuarios.csv")
+response2 = s3.upload_file(ficheroUpload2, nombreBucket, "direccion/direccion.csv")
 
 print("Ingesta completada")
